f3250867746665f2981e2362567c877d8a94719f
qwqVictor <i@qwq.ren>
2023-04-16 20:49:52 +0800

    style(as2): style printf

--- Assignment2/main.c
+++ Assignment2/main.c
@@ -7,7 +7,8 @@ int main(int argc, char const *argv[]) {
     case 0:
         pid_t parentpid = getppid();
         pid_t thispid = getpid();
-        printf("Child: My pid = %d. My parent has pid = %d\nChild: The current user is:\n", thispid, parentpid);
+        printf("Child: My pid = %d. My parent has pid = %d\nChild: The current user is:\n", 
+                thispid, parentpid);
         return execv("/bin/whoami", (char* const[]){"whoami", NULL});
         break;
     case -1:
