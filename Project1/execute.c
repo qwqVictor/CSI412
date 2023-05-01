f8197c13f9126c70723073b3e27a2c876dfe087f
qwqVictor <i@qwq.ren>
2023-05-01 18:47:53 +0800

    docs(project1): add comments

--- Project1/execute.c
+++ Project1/execute.c
@@ -2,10 +2,12 @@
 
 void execute_command(char* command[], int input_fd, int output_fd) {
     pid_t pid = fork();
+    // check if fork is failed
     if (pid == -1) {
         perror("fork() failed");
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
+        // redirect stdin and stdout to fd
         if (input_fd != STDIN_FILENO) {
             dup2(input_fd, STDIN_FILENO);
             close(input_fd);
@@ -23,14 +25,18 @@ void execute_command(char* command[], int input_fd, int output_fd) {
 void execute_piped_command(char* commands[MAX_COMMANDS][MAX_ARGS], int pipes[][2], int num_commands) {
     for (int i = 0; i < num_commands; i++) {
         if (i < num_commands - 1) {
+            // make pipes
             if (pipe(pipes[i]) == -1) {
                 perror("pipe() failed");
                 exit(EXIT_FAILURE);
             }
         }
+        // specify input and output fd
         int input_fd = (i > 0) ? pipes[i - 1][0] : STDIN_FILENO;
         int output_fd = (i < num_commands - 1) ? pipes[i][1] : STDOUT_FILENO;
+        // execute command with specified fd
         execute_command(commands[i], input_fd, output_fd);
+        // close the previous fd
         if (i < num_commands - 1) {
             close(pipes[i][1]);
         }
--- Project1/parser.c
+++ Project1/parser.c
@@ -2,18 +2,27 @@
 
 void parse_command(char* command, char* commands[MAX_COMMANDS][MAX_ARGS], int* num_commands) {
     int command_length = 0;
+    // split token with spaces
     char* token = strtok(command, " \n");
     while (token != NULL) {
+        // check if the token is the pipe symbol
         if (strcmp(token, "|") == 0) {
+            // terminate the last command
             commands[(*num_commands)][command_length] = NULL;
+            // increase the command counter
             (*num_commands)++;
+            // reset command length
             command_length = 0;
         } else {
+            // save the command token
             commands[(*num_commands)][command_length] = token;
+            // increase the command length
             command_length++;
         }
+        // fetch the next token
         token = strtok(NULL, " \n");
     }
+    // terminate the last command
     commands[(*num_commands)][command_length] = NULL;
     (*num_commands)++;
 }
