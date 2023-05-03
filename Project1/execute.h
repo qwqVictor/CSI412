7176fb30598cbfdf03da92124697e809371d7744
qwqVictor <i@qwq.ren>
2023-05-03 14:11:54 +0800

    docs(project1): add method comments

--- Project1/execute.h
+++ Project1/execute.h
@@ -7,6 +7,23 @@
 #include <fcntl.h>
 #include "limits.h"
 
+/**
+ * Fork and execute commands
+ * @param command the command vector to be executed
+ * @param input_fd the file descriptor to replace stdin
+ * @param output_fd the file descriptor to replace stdout
+ * @return if error it should be -1, otherwise 0.
+ */
 int execute_command(char* command[], int input_fd, int output_fd);
+
+/**
+ * Make the pipeline and execute
+ * @param command the commands to be pipelined
+ * @param pipes the file descriptor array storage
+ * @param file_remap stdin and stdout redirection map
+ * @param num_commands the command num count
+ * @return if error it should be -1, otherwise 0.
+ */
 int execute_piped_command(char* commands[MAX_COMMANDS][MAX_ARGS], int pipes[][2], char* file_remap[2], int num_commands);
+
 #endif
--- Project1/parser.h
+++ Project1/parser.h
@@ -3,6 +3,14 @@
 
 #include <string.h>
 #include "limits.h"
+/**
+ * Parsing the command line arguments
+ * @param command the command string to be parsed
+ * @param commands the parsing result
+ * @param file_remap stdin and stdout redirection map result
+ * @param num_commands the command num counter result
+ * @return if error it should be -1, otherwise 0.
+*/
 int parse_command(char* command, char* commands[MAX_COMMANDS][MAX_ARGS], char* file_remap[2], int* num_commands);
 
 #endif
