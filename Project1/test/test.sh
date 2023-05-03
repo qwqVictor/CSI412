a36f010158aeefa8366eb7b18fb8021c350382e3
qwqVictor <i@qwq.ren>
2023-05-03 11:36:08 +0800

    refactor(project1): add test name

--- Project1/test/test.sh
+++ Project1/test/test.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 progTmp="$(mktemp -d)"
 sysTmp="$(mktemp -d)"
-testName="countryCity"
+testName="$1"
 cd "$(dirname "$0")/" || exit 1
+if [ ! -f "$testName.sh" ]; then
+    echo "Test suite not found!"
+    exit 1
+fi
 sh -c 'cd .. && make'
 cp -a "${testName}.sh" "${testName}"/* "${progTmp}/"
 cp -a "${testName}.sh" "${testName}"/* "${sysTmp}/"
