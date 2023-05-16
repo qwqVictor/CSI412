967817c403dbe77f4d33108f5de842f2305f96fd
qwqVictor <i@qwq.ren>
2023-05-16 21:32:02 +0800

    feat(as5): finish

--- Assignment5/client.c
+++ Assignment5/client.c
@@ -13,6 +13,7 @@
  * $
  *
  */
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -21,47 +22,115 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#define MAX_BUF_SIZE 1024
+
+enum { TO_UPPER,
+       COUNT };
+
 void error(const char* msg) {
     perror(msg);
     exit(0);
 }
+
 int main(int argc, char* argv[]) {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent* server;
-    char buffer[256];
-    if (argc < 3) {
+    int type = -1;
+    char buffer[MAX_BUF_SIZE];
+
+    if (argc < 2) {
         fprintf(stderr, "usage %s hostname port\n", argv[0]);
         exit(0);
     }
+
+    // 建立与服务器端的连接
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
+
     server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr, "ERROR, no such host\n");
         exit(0);
     }
+
     bzero((char*)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char*)server->h_addr,
           (char*)&serv_addr.sin_addr.s_addr,
           server->h_length);
     serv_addr.sin_port = htons(portno);
+
     if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer, 256);
-    fgets(buffer, 255, stdin);
-    n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0)
-        error("ERROR writing to socket");
-    bzero(buffer, 256);
-    n = read(sockfd, buffer, 255);
-    if (n < 0)
-        error("ERROR reading from socket");
-    printf("%s\n", buffer);
+
+    while (!feof(stdin)) {
+        printf("> ");
+        // 解析客户端提供的参数
+        bzero(buffer, MAX_BUF_SIZE);
+        fgets(buffer, MAX_BUF_SIZE, stdin);
+        if (buffer[0] == 't')
+            type = TO_UPPER;
+        else if (buffer[0] == 'c') {
+            type = COUNT;
+        }
+        // 构造客户端请求字符串，并发送给服务器端
+        n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0)
+            error("ERROR writing to socket");
+
+        // 接收服务器端的响应，并根据服务类型做出相应处理
+        bzero(buffer, MAX_BUF_SIZE);
+
+        if (type == COUNT) {
+            n = read(sockfd, buffer, MAX_BUF_SIZE - 1);
+            if (n < 0)
+                error("ERROR reading from socket");
+            printf("Count: %s\n", buffer);
+        } else if (type == TO_UPPER) {
+            // receive to buffer
+            n = read(sockfd, buffer, MAX_BUF_SIZE - 1);
+            if (n < 0)
+                error("ERROR reading from socket");
+            else if (n != 0 && n < 8) {
+                printf("ERROR parsing: n=%d,buffer=\"%s\"\n", n, buffer);
+                return 0;
+            }
+
+            // first: parse the file length
+            buffer[7] = 0;
+            int totalReceived = 0;
+            sscanf(buffer, "%d", &totalReceived);
+            if (totalReceived == 0)
+                return 0;
+            if (n == totalReceived + 8) {
+                totalReceived += totalReceived;
+                buffer[n] = '\0';
+                printf("%s", buffer + 8);
+            } else {
+                // handle sticky packet
+                totalReceived += n - 8;
+                printf("%s", buffer + 8);
+                totalReceived -= n - 8;
+                while (totalReceived > 0) {
+                    n = read(sockfd, buffer, MAX_BUF_SIZE - 1);
+                    buffer[n] = '\0';
+                    totalReceived += n;
+                    printf("%s", buffer);
+                    totalReceived -= n;
+                }
+            }
+            printf("\n");
+        } else {
+            error("ERROR unknown op");
+            return 0;
+        }
+    }
+
     close(sockfd);
+
     return 0;
 }
--- Assignment5/server.c
+++ Assignment5/server.c
@@ -13,6 +13,7 @@
  *
  */
 #include <arpa/inet.h>
+#include <ctype.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,48 +21,180 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#define MAX_BUF_SIZE 1024
+
+// 功能类型，固定为 toUpper 或 count
+enum { TO_UPPER,
+       COUNT };
+
+// 错误处理函数
 void error(const char* msg) {
     perror(msg);
     exit(1);
 }
+
+void makeTXTName(char* filename, char const *suffix, char* destination, int size) {
+    bzero(destination, size);
+    int len = strlen(filename), dot = len - 4;
+    if (strcmp(filename + dot, ".txt") == 0) {
+        strcpy(destination, filename);
+        destination[dot] = 0;
+    }
+    else return;
+    strcat(destination, suffix);
+}
+
+// 计算文件中某个字符的数量
+int countCharInFile(char* fileName, char c) {
+    int count = 0;
+    FILE* fp = fopen(fileName, "r");
+
+    if (fp == NULL)
+        error("ERROR open file");
+
+    char ch = fgetc(fp);
+    while (ch != EOF) {
+        if (ch == c)
+            count++; // 如果是要计数的字符，计数器加一
+        ch = fgetc(fp);
+    }
+
+    fclose(fp);
+    return count;
+}
+
 int main(int argc, char* argv[]) {
     int sockfd, newsockfd, portno;
     socklen_t clilen;
-    char buffer[256];
+    char buffer[MAX_BUF_SIZE];
     struct sockaddr_in serv_addr, cli_addr;
     int n;
+
     if (argc < 2) {
         fprintf(stderr, "ERROR, no port provided\n");
         exit(1);
     }
+
     fprintf(stdout, "Run client by providing host and port\n");
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
+
     bzero((char*)&serv_addr, sizeof(serv_addr));
     portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
-    if (bind(sockfd, (struct sockaddr*)&serv_addr,
-             sizeof(serv_addr)) < 0)
+
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR on binding");
+
     listen(sockfd, 5);
+
     clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd,
-                       (struct sockaddr*)&cli_addr,
-                       &clilen);
+    newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
+
     if (newsockfd < 0)
         error("ERROR on accept");
-    bzero(buffer, 256);
-    n = read(newsockfd, buffer, 255);
-    if (n < 0)
-        error("ERROR reading from socket");
-    printf("Here is the message: %s\n", buffer);
-    n = write(newsockfd, "I got your message", 18);
-    if (n < 0)
-        error("ERROR writing to socket");
+
+    while (1) {
+        // 接收并处理客户端发送的请求
+        bzero(buffer, MAX_BUF_SIZE);
+        n = read(newsockfd, buffer, MAX_BUF_SIZE - 1);
+
+        if (n < 0)
+            error("ERROR reading from socket");
+        char* token = strtok(buffer, "<,> "); // split string with strtok
+        int type = -1;                        // service type
+
+        char* tokens[MAX_BUF_SIZE];
+        char c = 0;
+        char* fileName = NULL;
+        bzero(tokens, sizeof(tokens));
+        // 解析客户端请求中的参数
+        int i = 0;
+        do {
+            tokens[i] = token;
+        } while ((token = strtok(NULL, "<,> ")) != NULL && (++i < MAX_BUF_SIZE));
+        if (strcmp(tokens[0], "count") == 0)
+            type = COUNT;
+        else if (strcmp(tokens[0], "toUpper") == 0)
+            type = TO_UPPER;
+        if (tokens[1])
+            fileName = tokens[1];
+        else
+            error("ERROR no filename");
+        char resultFileName[MAX_BUF_SIZE];
+        // switch case for different types
+        switch (type) {
+        case TO_UPPER:
+            FILE* inFP = fopen(fileName, "r");
+
+            if (inFP == NULL)
+                error("ERROR open file");
+
+            makeTXTName(fileName, "Upper.txt", resultFileName, MAX_BUF_SIZE);
+            fprintf(stderr, "resultFileName: %s\n", resultFileName);
+            FILE* outFP = fopen(resultFileName, "w");
+            if (outFP == NULL)
+                outFP = stdout;
+            char sendContent[MAX_BUF_SIZE];
+            bzero(sendContent, MAX_BUF_SIZE);
+            int i = 0;
+            // read from the file
+            // former 8 bytes is length description
+            for (char ch = fgetc(inFP); ch != EOF; ch = fgetc(inFP)) {
+                if (i >= MAX_BUF_SIZE - 8) {
+                    snprintf(sendContent, 7, "%d", i);
+                    fputs(sendContent, outFP);
+                    n = write(newsockfd, sendContent, i + 8);
+                    if (n < 0)
+                        error("ERROR writing to socket");
+                    bzero(sendContent, sizeof(sendContent));
+                }
+                sendContent[8 + i++] = toupper(ch);
+            }
+            int k = snprintf(sendContent, 7, "%d", i);
+            for (int j = k; j < 8; j++)
+                sendContent[j] = '\n';
+            fputs(sendContent, outFP);
+            fclose(outFP);
+            n = write(newsockfd, sendContent, i + 8);
+            if (n < 0)
+                error("ERROR writing to socket");
+            bzero(sendContent, sizeof(sendContent));
+            fclose(inFP);
+            break;
+
+        case COUNT:
+            if (tokens[2] != NULL)
+                c = tokens[2][0];
+            else
+                error("ERROR no character");
+            int count = countCharInFile(fileName, c);
+            
+            makeTXTName(fileName, "Char.txt", resultFileName, MAX_BUF_SIZE);
+            FILE* fp = fopen(resultFileName, "w");
+            if (fp == NULL)
+                fp = stdout;
+            char countStr[MAX_BUF_SIZE];
+            // int to string
+            snprintf(countStr, MAX_BUF_SIZE, "%d", count);
+            fputs(countStr, fp);
+            fclose(fp);
+            n = write(newsockfd, countStr, strlen(countStr));
+            if (n < 0)
+                error("ERROR writing to socket");
+            break;
+        default:
+            break;
+        }
+    }
+
     close(newsockfd);
     close(sockfd);
+
     return 0;
 }
